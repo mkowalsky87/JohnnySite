0ed7f3c12ce0c9113818938cbffad755431212bb
Mike Kowalsky <innoender@gmail.com>
2013-08-27 12:28:06 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JohnnySite::Application.config.secret_key_base = '114ab8ad290696320a5a1b40a8b61519b1cffe7d473ed35aedf6cb5a08636a54dd23e5a15aff9d926a411b47472f24fc8174036e89a2810440ec663df6485be1'
